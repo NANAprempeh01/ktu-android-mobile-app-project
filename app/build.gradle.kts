765efc2288a927bdc0a5b836ab71779dc89db22c
Nana Prempeh <nanaprempeh254@gmail.com>
2026-07-07 15:57:42 +0000

    Update screenshots and fix resource compilation issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.constraint_layout_assignment"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.constraint_layout_assignment"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
